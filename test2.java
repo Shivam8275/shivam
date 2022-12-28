94ae41f4de2518de6f8891252e125541d065063e
Shivam8275 <shivamjadhav9122000@gmail.com>
2022-12-28 18:52:49 +0530

    changed test2.java and commited here

--- test2.java
+++ test2.java
@@ -1,6 +1,6 @@
 public class Circle {
     public static void main(String[] args) {
-        
+        System.out.println("enter the radius ");
         System.out.println("enter the radius of circle");
 }
 }

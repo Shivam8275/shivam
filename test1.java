e869289a35701361d1310e56cecc55de3991f246
Shivam8275 <shivamjadhav9122000@gmail.com>
2022-12-28 18:13:55 +0530

    initial commit

---
+++ test1.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the radius of circle");
+        System.out.println("enter the name");
+}
+}

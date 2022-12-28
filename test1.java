fd5754096e8475926f30aeaa18a2577ebaaa81d5
Shivam8275 <shivamjadhav9122000@gmail.com>
2022-12-28 19:02:35 +0530

    remove from git - test1.java

--- test1.java
+++
@@ -1,7 +0,0 @@
-public class Circle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the radius of circle");
-        System.out.println("enter the name");
-}
-}

d84142bc8a26dd58b3cf91c00cd2af1d2fe136be
Shivam8275 <121437836+Shivam8275@users.noreply.github.com>
2022-12-28 21:01:41 +0530

    after adding

---
+++ file.java
@@ -0,0 +1,5 @@
+Class Shivam{
+public static void main(String[] args) {
+        System.out.println("enter the radius ");
+        x=10;
+}
---
+++ test3.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        System.out.println("enter the radius ");
+        System.out.println("enter the radius of circle");
+        y=30:
+}
+}
